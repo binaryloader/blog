f2b597867527957c6535d73ae45f0d7318c73287
binaryloader <binaryloader.official@gmail.com>
2024-06-23 13:31:09 +0900

    feat: 코드 복사하기 기능 추가

---
+++ assets/js/custom/copy-code-button.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const codeBlocks = document.querySelectorAll("pre > code");
+
+  codeBlocks.forEach(function (codeBlock) {
+    const button = document.createElement("button");
+    button.className = "copy-code-button";
+    button.type = "button";
+    button.innerHTML = '<i class="fas fa-copy"></i> 복사';
+
+    const pre = codeBlock.parentNode;
+    pre.insertBefore(button, codeBlock);
+
+    button.addEventListener("click", function () {
+      const code = codeBlock.innerText;
+      navigator.clipboard.writeText(code).then(
+        function () {
+          button.innerHTML = '<i class="fas fa-check"></i> 복사됨';
+          setTimeout(function () {
+            button.innerHTML = '<i class="fas fa-copy"></i> 복사';
+          }, 2000);
+        },
+        function (error) {
+          button.innerHTML = '<i class="fas fa-times"></i> 에러 발생함';
+          console.error("Failed to copy code: ", error);
+        }
+      );
+    });
+  });
+});
